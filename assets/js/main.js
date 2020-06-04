f91b7e43bcd9fd23be74a3f45e984f41d9d2c4f9
Yonnys Quintero <leafar.qr@gmail.com>
2020-06-04 10:23:27 -0400

    theme almanza

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+$(function () {
+  'use strict'
+
+  $('[data-toggle="offcanvas"]').on('click', function () {
+    $('.offcanvas-collapse').toggleClass('open')
+  })
+})
+
+$(function () {
+  'use strict'
+
+  $('.nav-link__mobile').on('click', function () {
+    $('.offcanvas-collapse').removeClass('open')
+    $('.hamburger').removeClass("is-active");
+  })
+})
+
+
+$('.panel-collapse').on('show.bs.collapse', function () {
+  $(this).siblings('.panel-heading').addClass('active');
+});
+
+$('.panel-collapse').on('hide.bs.collapse', function () {
+  $(this).siblings('.panel-heading').removeClass('active');
+});
+
+
+// Menú fixed
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 70 && ($(window).width() >= 0)) {
+    $('.navbar-fixed-js').addClass('fixed');
+    $('.nav-link').addClass('fixed-color');
+    $('.nav-link--btn').addClass('fixed-color--btn');
+    $('.nav-top__header').addClass('nav-top__header--detele');
+    $('.hamburger-inner').addClass('js-hamburger');
+  
+
+  } else {
+    $('.navbar-fixed-js').removeClass('fixed');
+    $('.nav-link').removeClass('fixed-color');
+    $('.nav-link--btn').removeClass('fixed-color--btn');
+    $('.nav-top__header').removeClass('nav-top__header--detele');
+    $('.hamburger-inner').removeClass('js-hamburger');
+  }
+});
+
+// menu hambuger
+
+$(".hamburger").on("click", function () {
+  if (!$(this).hasClass("is-active")) {
+    $(this).addClass("is-active")
+    $('.navbar-fixed-js').addClass('fixed');
+    $('.hamburger-inner').addClass('js-hamburger');
+    $('.nav-link').addClass('fixed-color');
+  } else {
+    $(this).removeClass("is-active")
+    if ($(document).scrollTop() <= 70 && ($(window).width() >= 0)) {
+      $('.navbar-fixed-js').removeClass('fixed');
+      $('.hamburger-inner').removeClass('js-hamburger');
+      $('.nav-link').removeClass('fixed-color');
+
+    }
+  }
+});
